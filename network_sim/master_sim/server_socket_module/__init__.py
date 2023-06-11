d1133aee038158eb3ec36fc182b873232e281021
Thiago Andrade <thiago.vieira@ufba.br>
2023-06-11 02:05:44 -0300

    fix: add agora faz busca por arquivos guardados

--- network_sim/master_sim/server_socket_module/__init__.py
+++ network_sim/master_sim/server_socket_module/__init__.py
@@ -1,33 +1,123 @@
 import socket
-from multiprocessing import Process  
+from multiprocessing import Process, Lock,Manager
+from .stream_handler import StreamHandler,StreamType  
+from .stream_handler.stream_socket_utils import * 
+
 class ServerSocketError (Exception):
-    def __init__(self, type:int = 0) -> None:
-        self.errors = ["Erro 0: Erro não especificado", "Erro 1: Erro de conexão do socket"]
-        super().__init__(self.errors[type])
+    def __init__(self, typeError:int = 0) -> None:
+        banner = f"[ServerSocketError {typeError}] "
+        self.errors = ["Erro não especificado", "Erro de conexão do socket"]
+        fnMsg = banner + self.errors[typeError]
+        super().__init__(fnMsg)
         pass
 
 class ServerSocket():
-    def __init__(self, ip:int, port:int) -> None:
-        self.s = socket.create_server((ip,port), family=socket.AF_INET)
+    _addcmd = "0".encode("ascii")
+    _listcmd = "1".encode("ascii")
+    _modcmd = "2".encode("ascii")
+    _rmcmd = "3".encode("ascii")
+    _addminioncmd = "9".encode("ascii")
+    def __init__(self, ip:int, porta:int) -> None:
+        self.reg_list = open("./.registerlist", "a+b")
+        self.s = socket.create_server((ip,porta), family=socket.AF_INET)
+        self.num_repl = 0 
+        self.round_robin = 0
 
-    def handle_command(self, c):
-        fmsg = c.recv(1)
-        fmsg = fmsg.decode("ascii")
-        print(fmsg)
-        if fmsg == "0":
-            len_nome_arq = c.recv(8)
-            len_nome_arq = int.from_bytes(len_nome_arq, 'little') 
-            nome_arq = c.recv(len_nome_arq)
-            nome_arq = nome_arq.decode("ascii") 
-            print(nome_arq)
-            c.shutdown(socket.SHUT_RDWR)
-            c.close()
+    def close_connection(self,c:socket):
+        c.shutdown(socket.SHUT_RDWR)
+        c.close()
+
+    def send_header_to_minion(self, header, m:socket):
+        for msg in header:
+            if type(msg) is str:
+                socket_send_str(m, msg)
+            elif type(msg) is int:
+                socket_send_int(m, msg)
+            else:
+                m.send(msg)
+
+    def send_to_minion(self, msg, m):
+        try:
+            m.send(msg)
+        except BrokenPipeError as e:
+            pass
     
+    def existe_em_registro(self, chave:str, decoder:str = 'ascii'):
+        self.reg_list.seek(0)
+        for line in self.reg_list:
+            nome_arq = line.decode(decoder).split(",")[0]
+            if chave == nome_arq:
+                return True
+        return False
+        
+
+    def add_cmd(self,c):
+        nome_arq = socket_recv_str(c) 
+        if self.existe_em_registro(nome_arq):
+            self.close_connection(c)
+            return
+        tam_arq = socket_rect_int(c)
+        num_repl = socket_rect_int(c)
+        minion_msg = [self._addcmd]
+        minion_msg.append(nome_arq)
+        minion_msg.append(tam_arq)
+        pipe = StreamHandler(streamType=StreamType.Pipe, tam_arq=tam_arq)
+        if len(self.minion_list) <= num_repl:
+            process_list = [Process(target=self.send_header_to_minion, args=(minion_msg,m)) for (hs,m) in self.minion_list]
+            registro = f"{nome_arq},"
+            for (hs,m) in self.minion_list:
+                registro = registro + hs + ","
+            registro = registro + "\n"
+            self.reg_list.write(registro.encode('ascii'))
+            for p in process_list:
+                p.start()
+            for p in process_list:
+                p.join()
+            for j in pipe:
+                msg = c.recv(j)
+                print(f"Mensagem do client: {msg}")
+                #SEND STREAM 
+                process_list = [Process(target=self.send_to_minion, args=(msg,m)) for (hs,m) in self.minion_list]
+                for p in process_list:
+                    p.start()
+                for p in process_list:
+                    p.join()
+        else:
+            servers_list = [self.minion_list[i%len(self.minion_list)] for i in range(self.round_robin, num_repl)] 
+            registro = f"{nome_arq},"
+            for (hs,m) in servers_list:
+                registro = registro + hs + ","
+                p = Process(target=self.send_header_to_minion, args=(minion_msg,m))
+                p.start()
+            registro = registro + "\n"
+            self.reg_list.write(registro.encode('ascii'))
+            for (hs,m) in servers_list:
+                p.join()
+            for j in pipe:
+                msg = c.recv(j)
+                for (hs,m) in servers_list:
+                    p = Process(target=self.send_to_minion, args=(minion_msg,m))
+                    p.start()
+                for (hs,m) in servers_list:
+                    p.join()
+
+    def add_minion(self, c:socket, addr):
+        hostnameminion = socket_recv_str(c)
+        self.minion_list.append((hostnameminion,c))
+
     def start_server(self):
-        self.s.listen(256)
-        while True:
-            c, addr = self.s.accept()
-            p = Process(target=self.handle_command, args=(c,))
-            p.start()
+        self.s.listen(0)
+        with Manager() as manager:
+            self.minion_list = []
+            while True:
+                print(self.minion_list)
+                c, addr = self.s.accept()
+                fmsg = c.recv(1)
+                print(fmsg)
+                if fmsg == self._addcmd:
+                    self.add_cmd(c)
+                if fmsg == self._addminioncmd:
+                    self.add_minion(c, addr)
+
 
 
